86fbd4dc39ef460b187bb72a47527ba149e59299
徐兴 <xuxing@hynn.com.cn>
2023-04-03 16:32:19 +0800

    first commit

---
+++ excel2json.ts
@@ -0,0 +1,42 @@
+const xlsx = require("node-xlsx");
+
+import path from "path";
+import fs from "fs";
+
+// const sheets = xlsx.parse(path.join(__dirname, "./src/excel/国际化.xlsx"));
+const sheets = xlsx.parse("./public/xlsx/国际化.xlsx");
+
+const sheetsData = sheets[0].data;
+
+let files: { [key: string]: any } = {};
+let headers: string[] = [];
+sheetsData.forEach((item: string[], index: Number) => {
+  if (index === 0) {
+    headers = item;
+
+    const fileList = [...item];
+    fileList.shift();
+    fileList.forEach((fileName: string) => {
+      files[fileName] = {};
+    });
+  } else {
+    let key: string;
+    item.forEach((name: string, index: number) => {
+      if (index === 0) {
+        key = name;
+      } else {
+        files[headers[index]][key] = name;
+      }
+    });
+  }
+});
+
+// console.log(sheets);
+
+for (const filename of Object.keys(files)) {
+  // 导出json文件
+  fs.writeFileSync(
+    path.join(__dirname, `./public/json/${filename}.json`),
+    JSON.stringify(files[filename], null, "\t")
+  );
+}
---
+++ json2excel.ts
@@ -0,0 +1,76 @@
+const json2xls = require("json2xls");
+
+import path from "path";
+import fs from "fs";
+const getPathInfo = (p: string) => path.parse(p);
+/**
+ * @description // 递归读取文件，类似于webpack的require.context()
+ * @param {String} directory 文件目录
+ * @param {Boolean} useSubdirectories 是否查询子目录，默认false
+ * @param {array} extList 查询文件后缀，默认 ['.js']
+ *
+ */
+function autoLoadFile(
+  directory: string,
+  useSubdirectories = false,
+  extList = [".js"]
+) {
+  const filesList: string[] = [];
+  // 递归读取文件
+  function readFileList(
+    directory: string,
+    useSubdirectories: boolean,
+    extList: string[]
+  ) {
+    const files = fs.readdirSync(directory);
+    files.forEach((item: string) => {
+      const fullPath = path.join(directory, item);
+      const stat = fs.statSync(fullPath);
+      if (stat.isDirectory() && useSubdirectories) {
+        readFileList(path.join(directory, item), useSubdirectories, extList);
+      } else {
+        const info = getPathInfo(fullPath);
+        extList.includes(info.ext) && filesList.push(fullPath);
+      }
+    });
+  }
+  readFileList(directory, useSubdirectories, extList);
+  // 生成需要的对象
+  const res = filesList.map((item: string) => ({
+    path: item,
+    data: require(item),
+    ...getPathInfo(item),
+  }));
+
+  return res;
+}
+// 自动获取同目录下的json文件夹下的json文件
+const fileList = autoLoadFile(path.join(__dirname, "./src/i18n"), true, [
+  ".json",
+]);
+
+let data: any = []; // json2xls需要的数据格式: [{zh: "我是中文", en: "i am chinese"}]
+fileList.forEach((_, i) => {
+  // _.data = {Kxxxx:'语言'}
+  for (const [key, val] of Object.entries(_.data)) {
+    if (i === 0) {
+      // 首次遍历新建对应键名
+      data.push({ 键名: key, [_.name]: val });
+    } else {
+      // 非首次遍历匹配对应键名新增数据
+      data.forEach((e: any) => {
+        if (e["键名"] === key) {
+          e[_.name] = val;
+        }
+      });
+    }
+  }
+});
+
+// 导出excel文件
+fs.writeFileSync(
+  path.join(__dirname, "./public/xlsx/国际化.xlsx"),
+  json2xls(data),
+  "binary"
+);
+
