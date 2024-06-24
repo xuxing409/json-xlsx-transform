f11711bac56c161b741520fc7d5e368859828b72
徐兴 <xuxing@hynn.com.cn>
2024-06-24 15:26:09 +0800

    refactor: 重构读取的源excel文件名

--- src/excel2json.ts
+++ src/excel2json.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 import path from "path";
 
 const workbook = XLSX.readFile(
-  path.join(__dirname, "./source/国际化(MES前端)-EN 20230705.xlsx")
+  path.join(__dirname, "./source/国际化-翻译版.xlsx")
 );
 
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
